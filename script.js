ad82174f0d81051b924de61408778fbcd666d140
Raza <mdraza78601@gmail.com>
2026-07-13 15:38:46 +0530

    kjbksd

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const HINT = "The day we became us 🗝️";
 const GREETING = "It's your special day! 🎉";
 
 const MEMORIES = [
-  { src: "Img/memor-y1.jpg", caption: "golden hours ☁️", tilt: -5 },
+  { src: "Img/memory-1.jpg", caption: "golden hours ☁️", tilt: -5 },
   { src: "Img/memory-2.jpg", caption: "sweetest days 🍦", tilt: 3 },
   { src: "Img/memory-3.jpg", caption: "party mode 🎈", tilt: -2 },
   { src: "Img/memory4.jpg", caption: "you & flowers 🌷", tilt: 4 },
